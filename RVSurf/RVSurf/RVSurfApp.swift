f0722e5d30c68cc9ec7e6b7c82ce7b1e15f93b2f
radcli14 <radcli14@gmail.com>
2026-08-03 20:01:10 +0200

    empty template

---
+++ RVSurf/RVSurf/RVSurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  RVSurfApp.swift
+//  RVSurf
+//
+//  Created by Eliott Radcliffe on 8/3/26.
+//
+
+import SwiftUI
+
+@main
+struct RVSurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
