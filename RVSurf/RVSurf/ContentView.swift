aafdf9b7bb02073bc092ba8b80777bd77890a16f
radcli14 <radcli14@gmail.com>
2026-08-04 19:11:54 +0200

    working version, trying to figure out how to get plane detection to fire for non-lidar phones

--- RVSurf/RVSurf/ContentView.swift
+++ RVSurf/RVSurf/ContentView.swift
@@ -9,19 +9,12 @@ import RealityKit
 import SwiftUI
 
 struct ContentView: View {
+    @State private var session = SpatialTrackingSession()
     
     var body: some View {
         RealityView { content in
             content.camera = .spatialTracking
-            
-            // Subscribe to events associated with real-world LiDAR surfaces being added.
-            // Using ModelComponent.self assures that their mesh has been generated.
-            let _ = content.subscribe(
-                to: ComponentEvents.DidAdd.self,
-                componentType: ModelComponent.self
-            ) { event in
-                event.entity.components[ModelComponent.self]?.materials = [material]
-            }
+            content.setupSpatialTrackingVisualization()
         }
         .task(startSession)
     }
@@ -29,18 +22,47 @@ struct ContentView: View {
     // - MARK: - Initialization
     
     private func startSession() async {
-        let session = SpatialTrackingSession()
         let config = SpatialTrackingSession.Configuration(
             tracking: [.camera, .plane, .object, .world],
             sceneUnderstanding: [.occlusion, .physics, .collision, .shadow]
         )
         await session.run(config)
     }
+}
+
+extension RealityViewCameraContent {
     
-    // - MARK: - Constants
+    func setupSpatialTrackingVisualization() {
+        subscribeToLidarSurfaceEvents()
+        subscribeToPlaneDetectionEvents()
+    }
     
-    private let material: UnlitMaterial = {
-        var mat = UnlitMaterial(color: .green)
+    /// Subscribe to events associated with real-world LiDAR surfaces being added.
+    /// Using ModelComponent.self assures that their mesh has been generated.
+    private func subscribeToLidarSurfaceEvents() {
+        let _ = subscribe(
+            to: ComponentEvents.DidAdd.self,
+            componentType: ModelComponent.self
+        ) { event in
+            event.entity.components[ModelComponent.self]?.materials = [Self.meshTrackingMaterial]
+        }
+    }
+    
+    // TODO: intended to capture simple plane detection on non-LiDAR devices, but the second print is never firing.
+    private func subscribeToPlaneDetectionEvents() {
+        print("subscribing to plane detection")
+        let _ = subscribe(
+            to: ComponentEvents.DidAdd.self,
+            componentType: AnchoringComponent.self
+        ) { event in
+            print("anchor event", event)
+        }
+    }
+    
+    private static let meshTrackingMaterial: PhysicallyBasedMaterial = {
+        var mat = PhysicallyBasedMaterial()
+        mat.baseColor = .init(tint: .green)
+        mat.emissiveColor = .init(color: .green)
         mat.triangleFillMode = .lines
         return mat
     }()
